d8984852a22e2b002f7be8d861af3167f15063ba
style77 <stylek777@gmail.com>
2024-03-24 03:36:14 +0100

    chore: supabase integration, login and signup functionality

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,7 @@
+'use client'
+
 import Navbar from "@/components/Navbar";
-import React from "react";
+import React, { useState } from "react";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/utils/cn";
@@ -10,18 +12,83 @@ import {
 } from "@tabler/icons-react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useRouter } from 'next/navigation'
+
+// import type { Database } from '@/lib/database.types'
+
+
 export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [confirmPassword, setConfirmPassword] = useState('')
+
+  const [error, setError] = useState('')
+  const [success, setSuccess] = useState('')
+
+  const router = useRouter()
+  const supabase = createClientComponentClient() // <Database>
+
+  const clearMessages = () => {
+    setError('')
+    setSuccess('')
+  }
+
+  const validatePasswords = () => {
+    if (password !== confirmPassword) {
+      setError('Passwords do not match.')
+      return false
+    }
+    return true
+  }
+
+  const handleSignUp = async () => {
+    clearMessages()
+
+    if (!validatePasswords()) {
+      setError('Passwords do not match.')
+    }
+
+    const response = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/auth/callback`,
+      },
+    })
+    if (response.error) {
+      setError(response.error.message)
+      return
+    }
+
+    setSuccess('Account created! Check your email for confirmation link. 🎉')
+    router.refresh()
+  }
+
+  const handleSignIn = async () => {
+    const response = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+    if (response.error) {
+      setError(response.error.message)
+    }
+
+    router.refresh()
+  }
+
   return (
     <main className="flex flex-col">
       <Navbar />
       <div className="flex flex-col h-full items-center justify-center w-full">
-        <div className="flex flex-col bg-white/5 shadow-xl w-96 h-min rounded-lg mt-10 shadow-xl p-6">
-          <Tabs defaultValue="account" className="">
-            <TabsList className="flex flex-row justify-center bg-transparent">
-              <TabsTrigger value="password">Sign In</TabsTrigger>
-              <TabsTrigger className="" value="account">Sign Up</TabsTrigger>
+        <div className="flex flex-col bg-white/5 shadow-xl w-96 h-min rounded-lg mt-10 p-6">
+          <Tabs defaultValue="signin">
+            <TabsList className="flex flex-row justify-center bg-transparent gap-2">
+              <TabsTrigger value="signin" onClick={() => setError("")}>Sign In</TabsTrigger>
+              <TabsTrigger value="signup" onClick={() => setError("")}>Sign Up</TabsTrigger>
             </TabsList>
-            <TabsContent className="z-10" value="account">
+            <TabsContent className="z-10" value="signup">
               <div className="flex flex-col w-full justify-center z-10">
                 <h2 className="font-bold text-xl text-neutral-200">
                   Welcome
@@ -29,41 +96,39 @@ export default function Login() {
                 <p className="text-sm max-w-sm mt-2 text-neutral-300">
                   Fill up the from and press sign up button to create account.
                 </p>
-                <form className="my-8 z-10">
-                  <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-                    <LabelInputContainer>
-                      <Label htmlFor="firstname">First name</Label>
-                      <Input className="bg-[#1d1d34]" id="firstname" placeholder="Tyler" type="text" />
-                    </LabelInputContainer>
-                    <LabelInputContainer>
-                      <Label htmlFor="lastname">Last name</Label>
-                      <Input className="bg-[#1d1d34]" id="lastname" placeholder="Durden" type="text" />
-                    </LabelInputContainer>
-                  </div>
+                <div className="my-8 z-10">
                   <LabelInputContainer className="mb-4">
                     <Label htmlFor="email">Email Address</Label>
-                    <Input className="bg-[#1d1d34]" id="email" placeholder="projectmayhem@fc.com" type="email" />
+                    <Input className="bg-[#1d1d34]" id="email" placeholder="projectmayhem@fc.com" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
                   </LabelInputContainer>
                   <LabelInputContainer className="mb-4">
                     <Label htmlFor="password">Password</Label>
-                    <Input className="bg-[#1d1d34]" id="password" placeholder="••••••••" type="password" />
+                    <Input className="bg-[#1d1d34]" id="password" placeholder="••••••••" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                   </LabelInputContainer>
                   <LabelInputContainer className="mb-8">
-                    <Label htmlFor="twitterpassword">Repeat password</Label>
+                    <Label htmlFor="confirmpassword">Repeat password</Label>
                     <Input
                       className="bg-[#1d1d34]"
-                      id="twitterpassword"
+                      id="confirmpassword"
                       placeholder="••••••••"
-                      type="twitterpassword"
+                      type="password"
+                      value={confirmPassword}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
                     />
                   </LabelInputContainer>
 
                   <button
                     className="bg-indigo-500 relative group/btn block w-full text-white rounded-md h-10 font-medium"
-                    type="submit"
+                    onClick={handleSignUp}
                   >
                     Sign up &rarr;
                   </button>
+                  <span className="text-red-500 text-sm font-extralight">
+                    {error}
+                  </span>
+                  <span className="text-green-500 text-sm font-extralight">
+                    {success}
+                  </span>
 
                   <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent my-8 h-[1px] w-full" />
 
@@ -71,34 +136,37 @@ export default function Login() {
                     <IconBrandGithub className="h-6 w-6 text-neutral-300" />
                     <IconBrandGoogle className="h-6 w-6 text-neutral-300" />
                   </div>
-                </form>
+                </div>
                 <div className="absolute top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%] w-96 h-96 rounded-full blur-[300px] bg-indigo-500 z-0"></div>
               </div>
             </TabsContent>
-            <TabsContent value="password">
-            <div className="flex flex-col w-full justify-center">
+            <TabsContent value="signin">
+              <div className="flex flex-col w-full justify-center">
                 <h2 className="font-bold text-xl text-neutral-200">
                   Welcome
                 </h2>
                 <p className="text-sm max-w-sm mt-2 text-neutral-300">
                   Fill up the from and press sign in button to log into account.
                 </p>
-                <form className="my-8 z-10">
+                <div className="my-8 z-10">
                   <LabelInputContainer className="mb-4">
                     <Label htmlFor="email">Email Address</Label>
-                    <Input className="bg-[#1d1d34]" id="email" placeholder="projectmayhem@fc.com" type="email" />
+                    <Input className="bg-[#1d1d34]" id="email" placeholder="projectmayhem@fc.com" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
                   </LabelInputContainer>
                   <LabelInputContainer className="mb-8">
                     <Label htmlFor="password">Password</Label>
-                    <Input className="bg-[#1d1d34]" id="password" placeholder="••••••••" type="password" />
+                    <Input className="bg-[#1d1d34]" id="password" placeholder="••••••••" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                   </LabelInputContainer>
 
                   <button
                     className="bg-indigo-500 relative group/btn block w-full text-white rounded-md h-10 font-medium"
-                    type="submit"
+                    onClick={handleSignIn}
                   >
                     Sign in &rarr;
                   </button>
+                  <span className="text-red-500 text-sm font-extralight">
+                    {error}
+                  </span>
 
                   <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent my-8 h-[1px] w-full" />
 
@@ -106,7 +174,7 @@ export default function Login() {
                     <IconBrandGithub className="h-6 w-6 text-neutral-300" />
                     <IconBrandGoogle className="h-6 w-6 text-neutral-300" />
                   </div>
-                </form>
+                </div>
                 <div className="absolute top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%] w-96 h-96 rounded-full blur-[300px] bg-indigo-500 z-0"></div>
               </div>
             </TabsContent>
---
+++ app/middleware.ts
@@ -0,0 +1,21 @@
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
+
+import type { NextRequest } from 'next/server'
+// import type { Database } from '@/lib/database.types'
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+
+  const supabase = createMiddlewareClient({ req, res }) // <Database>
+
+  await supabase.auth.getSession()
+
+  return res
+}
+
+export const config = {
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico).*)',
+  ],
+}
