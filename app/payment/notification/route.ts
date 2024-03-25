81cc44e3f8699ab674e3100408d0d9768d97a649
style77 <stylek777@gmail.com>
2024-03-25 16:29:20 +0100

    chore: notification route base

---
+++ app/payment/notification/route.ts
@@ -0,0 +1,10 @@
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { NextResponse } from 'next/server'
+
+import type { NextRequest } from 'next/server'
+import type { Database } from '@/lib/database.types'
+
+export async function POST(request: NextRequest) {
+
+}
